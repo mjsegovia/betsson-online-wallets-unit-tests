6365b7543b3b833a29985d1fbf9c8e35ccac1260
mjsegovia <mj.segovia22@gmail.com>
2024-10-22 23:32:14 +0200

    add http client class to test the api in wallet services

---
+++ tests/Betsson.OnlineWallets.ApiTests/Services/ApiClient.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System.Security.Policy;
+using System.Text;
+
+namespace Betsson.OnlineWallets.Web.Services
+{
+    public class ApiClient : IApiClient
+    {
+        private readonly RestClient _client;
+
+        public ApiClient()
+        {
+            _client = new RestClient(new Uri("http://localhost/onlinewallet/")); 
+           
+        }
+
+        public async Task<RestResponse> GetAsync(string url)
+        {
+            var request = new RestRequest(url, Method.Get);
+            var response = await _client.ExecuteAsync(request);
+
+            HandleErrors(response);
+
+            return response;
+        }
+
+        public async Task<T> GetAsync<T>(string url)
+        {
+            var request = new RestRequest(url, Method.Get);
+            var response = await _client.ExecuteAsync(request);
+
+            HandleErrors(response);
+
+            var content = response.Content; 
+              
+            var responseData = JsonConvert.DeserializeObject<T>(content);
+            
+            return responseData;
+        }
+
+        public async Task<RestResponse> PostAsync<T>(string url, T payload) where T : class
+        {
+            var request = new RestRequest(url, Method.Post);
+            request.AddJsonBody(payload);
+
+            var response = await _client.ExecuteAsync(request);
+           // HandleErrors(response);
+
+            var content = response.Content;
+            var responseData = JsonConvert.DeserializeObject<T>(content);
+
+            
+
+            return response;
+        }
+
+        public async Task<string> GetResponseContent(RestResponse response)
+            => response.Content ?? throw new HttpRequestException($"Response content is null for the request.");
+       
+
+        //Private method to handle errors
+        private void HandleErrors(RestResponse response)
+        {
+            if (!response.IsSuccessful)
+                throw new SystemException($"Error: {response.StatusCode} - {response.ErrorMessage}");
+        }
+    }
+}
---
+++ tests/Betsson.OnlineWallets.ApiTests/Services/IApiClient.cs
@@ -0,0 +1,15 @@
+﻿using RestSharp;
+
+namespace Betsson.OnlineWallets.Web.Services
+{
+    public interface IApiClient
+    {
+        Task<RestResponse> GetAsync(string url);
+
+        Task<T> GetAsync<T>(string url);
+
+        Task<RestResponse> PostAsync<T>(string url, T payload) where T : class;
+
+        Task<string> GetResponseContent(RestResponse response);
+    }
+}
