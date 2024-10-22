5cc136a266b32f221f98f65d7854c0268ebf69a2
mjsegovia <mj.segovia22@gmail.com>
2024-10-22 23:35:11 +0200

    register dependenci injection for the client service

---
+++ tests/Betsson.OnlineWallets.ApiTests/Hooks/Hooks.cs
@@ -0,0 +1,22 @@
+﻿using Betsson.OnlineWallets.Web.Services;
+using BoDi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Betsson.OnlineWallets.ApiTests.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+        [BeforeScenario]
+        public void RegisterDependencies(IObjectContainer container)
+        {
+            // Register IApiClient with its implementation
+            container.RegisterTypeAs<ApiClient, IApiClient>();
+        }
+    }
+}
