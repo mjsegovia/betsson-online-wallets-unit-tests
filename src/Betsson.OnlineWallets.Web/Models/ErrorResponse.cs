7253ffce18927025a5cb7f073b75b2ef73a6e448
mjsegovia <mj.segovia22@gmail.com>
2024-10-22 23:27:44 +0200

    create model for responses with Bad Request

---
+++ src/Betsson.OnlineWallets.Web/Models/ErrorResponse.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Betsson.OnlineWallets.Web.Models
+{
+    public class ErrorResponse
+    {
+        [JsonProperty("type")]
+        public string? Type { get; set; }
+
+        [JsonProperty("title")]
+        public string? Title { get; set; }
+
+        [JsonProperty("status")]
+        public int Status { get; set; }
+
+        [JsonProperty("errors")]
+        public Dictionary<string, List<string>> Errors { get; set; }
+
+        [JsonProperty("traceId")]
+        public string? TraceId { get; set; }
+    }
+}
