c57d237bc8908d18a64f4f5c808271e84e9b9d61
mjsegovia <mj.segovia22@gmail.com>
2024-10-23 13:47:19 +0200

    add config file for urls and static clas for scenario context keys

---
+++ tests/Betsson.OnlineWallets.ApiTests/Config/Configurator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace IconTestFramework.Core.Config
+{
+    public static class Configurator
+    {
+        public static IConfigurationRoot Config = new ConfigurationBuilder()
+                                                    .SetBasePath(Directory.GetCurrentDirectory() + @"..\..\..\..\..\Betsson.OnlineWallets.ApiTests\Config")
+                                                    .AddJsonFile("appsettings.json")
+                                                    .Build();
+
+        public static string BaseUrl => GetAppSettings("base_url");
+        public static string DepositUrl => GetAppSettings("deposit_url");
+        public static string BalanceUrl => GetAppSettings("balance_url");
+        public static string WithdrawalUrl => GetAppSettings("withdrawal_url");
+
+        private static string GetAppSettings(string name) => Config["AppSettings:" + name];
+    }
+}
---
+++ tests/Betsson.OnlineWallets.ApiTests/Config/ScenarioContextKey.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betsson.OnlineWallets.ApiTests.Config
+{
+    public static class ScenarioContextKeys
+    {
+        public const string EndPoint = "EndPoint";
+        public const string ResponseContent = "ResponseContent";
+        public const string DepositRequest = "DepositRequest";
+        public const string WithdrawalRequest = "WithdrawalRequest";
+        public const string CurrentBalance = "CurrentBalance";
+        public const string ErrorResponse = "ErrorResponse";
+        public const string InitialBalance = "InitialBalance";
+    }
+}
